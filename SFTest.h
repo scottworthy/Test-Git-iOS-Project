96b01ac01e7efe02408ba304eafab233e60ee5fa
Scott Feil <scottfeil@Staff-Scott-Feil.local>
2014-03-28 14:26:55 -0700

    added test class

---
+++ SFTest.h
@@ -0,0 +1,13 @@
+//
+//  SFTest.h
+//  Git Test
+//
+//  Created by Scott Feil on 3/28/14.
+//  Copyright (c) 2014 Scott Feil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFTest : NSObject
+
+@end
---
+++ SFTest.m
@@ -0,0 +1,13 @@
+//
+//  SFTest.m
+//  Git Test
+//
+//  Created by Scott Feil on 3/28/14.
+//  Copyright (c) 2014 Scott Feil. All rights reserved.
+//
+
+#import "SFTest.h"
+
+@implementation SFTest
+
+@end
